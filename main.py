e1475c4ce644bb3e43b386186240a01d0503b83d
qijun.tong@wsl <stagcutlery@gmail.com>
2025-05-11 12:29:29 +0900

    mia

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import formulation.edge_diff
-import formulation.xy_boundbox
+from formulation import edge_diff, xy_boundbox
 from visualization import generate_data, visualize_solution
 
+# パラメータ
 F_NUM = 12  # グリッドサイズ
 P_NUM = 10  # プレイヤー数
 SCORE_RANGE = (1, 30)  # 各マスのスコア乱数範囲
@@ -11,8 +11,6 @@ MAX_SECONDS = 100  # ソルバーの打ち切りタイムリミット
 
 
 def main():
-    # パラメータ
-
     # データ生成
     grid, reqs = generate_data(F_NUM, P_NUM, cell_value_range=SCORE_RANGE, req_range=REQ_RANGE, seed=SEED)
     print("Generated grid:")
@@ -20,10 +18,12 @@ def main():
     print("Player requirements:", reqs)
 
     # 異なる定式化で結果を比較
-    assignment = formulation.edge_diff.solve_assignment(grid, reqs, max_seconds=MAX_SECONDS)
+    # 割り当てエリアの座標の幅を最小化
+    assignment = edge_diff.solve_assignment(grid, reqs, max_seconds=MAX_SECONDS)
     visualize_solution(grid, assignment, seconds=MAX_SECONDS, filename="output/minimize_edge_diff.svg")
 
-    assignment = formulation.xy_boundbox.solve_assignment(grid, reqs, max_seconds=MAX_SECONDS)
+    # 隣接エリアが異なる割り当てになる回数の最小化
+    assignment = xy_boundbox.solve_assignment(grid, reqs, max_seconds=MAX_SECONDS)
     visualize_solution(grid, assignment, seconds=MAX_SECONDS, filename="output/minimize_xy_boundbox.svg")
 
 
